8b58b719d58d1d71309e4e69a56d060db150445b
Peter Sharp <peter.victor.sharp@gmail.com>
2019-03-25 22:50:29 +1300

    added toggler for sub-menus

--- src/app/nav/view.js
+++ src/app/nav/view.js
@@ -14,13 +14,23 @@ const createNavItem = curry(function (active, item) {
     const i = createNavItem.lastIndex
     createNavItem.lastIndex += 1
     return html`<li id="navitem_${i}" class="nav-item ${active == item.url ? 'nav-item--active' : ''}">
-    <a href=${item.url}>${item.title}</a>
-    ${item.children 
-        ? html`<ul class="nav-item__children">
-        ${item.children.map(createNavItem(active))}
-        </ul>`
-        : ''}
+        ${item.children ? withChildren(active, item) : itemLink(item)}
     </li>`
 })
 
 createNavItem.lastIndex = 0
+
+const withChildren = function(active, item) {
+    return html`<details class="nav-item__toggler">
+    <summary>
+        ${itemLink(item)}
+    </summary>
+    <ul class="nav-item__children">
+        ${item.children.map(createNavItem(active))}
+    </ul>
+</details>`
+}
+
+const itemLink = function(item) {
+    return html`<a href=${item.url}>${item.title}</a>`
+}
