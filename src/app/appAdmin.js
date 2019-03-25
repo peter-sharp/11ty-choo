b70112e311a01964b56f8d8bfab1c2c964c3aae3
Peter Sharp <peter.victor.sharp@gmail.com>
2019-03-25 22:51:10 +1300

    added netlify CMS

---
+++ src/app/appAdmin.js
@@ -0,0 +1,18 @@
+import CMS from 'netlify-cms'
+
+import Page from './preview-templates/page.js'
+
+CMS.registerPreviewTemplate("pages", Page);
+
+// Register any CSS file on the home page as a preview style
+fetch("/")
+  .then(response => response.text())
+  .then(html => {
+    const f = document.createElement("html");
+    f.innerHTML = html;
+    Array.from(f.getElementsByTagName("link")).forEach(tag => {
+      if (tag.rel == "stylesheet" && !tag.media) {
+        CMS.registerPreviewStyle(tag.href);
+      }
+    });
+});
